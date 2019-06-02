ab28e3e849ed6a71944b2d81836b65b71d85790c
Thomas75019 <tlarousse3@gmail.com>
2019-06-02 23:25:25 +0200

    Setting up doctrine

---
+++ cli-config.php
@@ -0,0 +1,10 @@
+
+
+<?php
+# cli-config.php
+
+$entityManager = require_once join(DIRECTORY_SEPARATOR, [__DIR__, 'bootstrap.php']);
+
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+return ConsoleRunner::createHelperSet($entityManager);
