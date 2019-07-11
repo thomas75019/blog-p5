d8b13febd90e1ddcb7fdba8b7f6e67077aeb567a
Thomas75019 <tlarousse3@gmail.com>
2019-07-11 14:04:36 +0200

    add Twig class

---
+++ src/Dependencies/Twig.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Blog\Dependencies;
+use Blog\Service\UserSession;
+
+/**
+ * Class Twig
+ *
+ * Set up twig and creates the method to get twig
+ *
+ * @package Blog\Dependencies
+ */
+class Twig
+{
+    /**
+     * @var \Twig\Loader\FilesystemLoader
+     */
+    public $loader;
+
+    /**
+     * @var \Twig\Environment
+     */
+    public $twig;
+
+    /**
+     * Twig constructor.
+     */
+    public function __construct(UserSession $session)
+    {
+        $this->loader = new \Twig\Loader\FilesystemLoader('/Users/thomaslarousse/Desktop/Projet5/src/View');
+
+        $this->twig = new \Twig\Environment($this->loader, [
+            'cache' => false
+        ]);
+
+        if ($session->isStored()) {
+            $this->twig->addGlobal('user', $session->get());
+        }
+    }
+
+    /**
+     * @return \Twig\Environment
+     */
+    public function getTwig()
+    {
+        return $this->twig;
+    }
+}
